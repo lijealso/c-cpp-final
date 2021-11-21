ac94968e54046f88ac17c71f9827191af19dece4
lijealso <lijealso@gmail.com>
2021-11-21 04:11:23 +0000

    v2

--- Forca.cpp
+++ Forca.cpp
@@ -1,78 +1,239 @@
 #include <iostream>
+#include <list>
 #include <cstdlib>
-#include <string>
 #include <ctime>
+#include <iterator>
+#include <algorithm>
+#include <vector>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
-string word, underscore, guess;
-int wrong = 0;
 
-string palavraSorteada;
-string palavraEscondida;
+list<string> :: iterator it;
+int tema_escolhido;
 int aleatorio;
+int aleatorio2;
+string palavra_escolhida;
+string palavra_escondida;
 string letra;
-int letraFalhadas = 0;
+int letrasFalhadas = 0;
 int letrasAcertadas = 0;
-int tentativasFalhadas = 0;
+int tentativasFalhadas;
+bool jogo_gravado;
 
-
-int main()
-
+string gravar_jogo()
 {
-    srand(time(NULL));
-    aleatorio = 1 + (rand() % 3); // gera aleatório para chave
-    cout << "Aleatorio gerado: " << aleatorio << endl; // mostra o aleatório
+    // cria e abre um ficheiro de texto
+	ofstream ficheiro("dados.txt");
 
+	// Escreve no ficheiro
+	ficheiro << tema_escolhido << "\n";
+	ficheiro << palavra_escolhida << "\n";
+    ficheiro << palavra_escondida << "\n";
+    ficheiro << tentativasFalhadas << "\n";
+    ficheiro << jogo_gravado;
 
-    if (aleatorio == 1)
+	// Fecha o ficheiro
+	ficheiro.close();
+
+    return "Dados gravados com sucesso...";
+}
+
+int carregarJogo()
+{
+
+    ifstream ficheiro ("dados.txt");
+
+        while ( !ficheiro.eof() )
     {
-        palavraSorteada = { 'b', 'a', 'n', 'a', 'n', 'a' };
-        palavraEscondida = { '*', '*', '*', '*', '*', '*' };
+        ficheiro >> tema_escolhido;
+        ficheiro >> palavra_escolhida;
+        ficheiro >> palavra_escondida;
+        ficheiro >> tentativasFalhadas;
+        ficheiro >> jogo_gravado;
     }
-    if (aleatorio == 2)
+
+    cout << "Tema: " << tema_escolhido << endl;
+    cout << "Palavra escolhida: " << palavra_escolhida << endl;
+    cout << "Palavra escondida: " << palavra_escondida << endl;
+    cout << "Tentativas falahadas: " << tentativasFalhadas << endl;
+    cout << "Jogo gravado: " << jogo_gravado << endl;
+
+    system("pause");}
+
+int menu_inicial()
+{
+    int opcao_menu_inicial;
+    do
     {
-        palavraSorteada = { 'g', 'o', 'i', 'a', 'b', 'a' };
-        palavraEscondida = { '*', '*', '*', '*', '*', '*' };
+        system("cls");
+        cout << "Bem-vindo(a) ao jogo da forca.\nEscolha um tema ou deixe que o programa decida:\n";
+        cout << "1 - Escolher tema\n";
+        cout << "2 - Definir aleatoriamente\n";
+        cout << "3 - Carregar jogo gravado\n";
+        cout << "0 - Sair\n";
+        cin >> opcao_menu_inicial;
+     }
+     while(opcao_menu_inicial < 0 || opcao_menu_inicial > 3 );
+
+     return opcao_menu_inicial;
+ }
+
+int escolher_tema()
+{
+    int tema;
+    vector<string> temas = { "Paises", "Nomes", "Frutos" };
+
+    cout << "Escolha um tema (numero): " << "\n";
+    for (int i = 0; i < temas.size(); i++)
+    {
+        cout << "Tema #" << (i+1) << ": " << temas[i] << endl;
+    }
+    cin >> tema;
+
+    cout << "Tema escolhido: " << temas[tema - 1] << endl;
+    return tema;
+ }
+
+int tema_aleatorio()
+{
+    srand((unsigned)time(0));
+    vector<string> temas = { "Paises", "Nomes", "Frutos" };
+    aleatorio = 1 + rand() % 3;
+
+    cout << "Aleatorio saido: " << aleatorio << endl;
+    system("pause");
+
+    for (int i = 0; i < temas.size(); i++)
+    {
+        if (i == aleatorio - 1)
+            cout << "O tema escolhido aleatoriamente foi: " << temas[aleatorio - 1] << endl;
     }
 
-    cout << "Palavra sorteada: " << endl;
-    cout << palavraSorteada << endl;
+    system("pause");
+    return aleatorio;
+}
 
+string escolher_palavra()
+{
+    srand((unsigned)time(0));
+    aleatorio2 = 1 + rand() % 10;
+
+    cout << "Aleatorio saido: " << aleatorio2 << endl;
+    system("pause");
+
+    if (tema_escolhido == 1)
+    {
+        // 10 países:
+        list<string> paises{"venezuela", "luxemburgo", "madagascar", "azerbaijao",
+        "bangladexe", "dinamarca", "eslovaquia", "filipinas", "mocambique", "montenegro"};
+        auto l_front = paises.begin();
+        advance(l_front, aleatorio2 - 1);
+        cout << *l_front << '\n';
+        cout << "Palavra escolhida (dentro da funcao): " << *l_front << '\n';
+        return *l_front;
+    }
+
+    if (tema_escolhido == 2)
+    {
+        // 10 nomes:
+        list<string> nomes{"auxiliadora", "rosangela", "lucineide", "valentina", "francisco",
+        "benjamin", "frederico", "querubim", "gertrude", "elisabete"};
+        auto l_front = nomes.begin();
+        advance(l_front, aleatorio2 - 1);
+        cout << *l_front << '\n';
+        cout << "Palavra escolhida (dentro da funcao): " << *l_front << '\n';
+        return *l_front;
+    }
+
+    if (tema_escolhido == 3)
+    {
+        // 10 frutos:
+        list<string> frutos{"castanha", "azeitona", "framboesa", "jabuticaba", "nectarina",
+        "tangerina", "tamarindo", "camarinha", "maracuja", "jenipapo"};
+        auto l_front = frutos.begin();
+        advance(l_front, aleatorio2 - 1);
+        cout << *l_front << '\n';
+        cout << "Palavra escolhida (dentro da funcao): " << *l_front << '\n';
+        return *l_front;
+    }
+}
+
+int jogo()
+{
+    cout << "Já estamos dentro da funcao jogo..." << endl;
+    cout << "A palavra escolhida foi: " << palavra_escolhida << endl;
+
+    cout << "tamanho da palavra escolhida: " << palavra_escolhida.length() << endl;
+
+    if (jogo_gravado == 0)
+    {
+    switch (palavra_escolhida.length())
+    {
+        case 8:
+            palavra_escondida = "********";
+            break;
+        case 9:
+            palavra_escondida = "*********";
+            break;
+        case 10:
+            palavra_escondida = "**********";
+            break;
+        case 11:
+            palavra_escondida = "***********";
+            break;
+    }
+    }
+
+    cout << "Palavra escondida: " << palavra_escondida << endl;
 
     while (tentativasFalhadas < 8)
     {
-        cout << "Acerte na palavra: " << palavraEscondida << endl;
+        char resposta;
+        cout << "Deseja gravar o jogo (s/n): ";
+        cin >> resposta;
+        system("pause");
+        if (resposta == 's')
+        {
+            jogo_gravado = 1;
+            string gravado = gravar_jogo();
+            cout << gravado << endl;
+        }
+        else
+        {
+            jogo_gravado = 0;
+        }
+        cout << "Acerte na palavra: " << palavra_escondida << endl;
         cout << "Pode falhar " << 8 - tentativasFalhadas << " vezes. " << endl;
 
-        cout << "Escolha uma letra:" << endl;
-        cin >> letra;
+            cout << "Escolha uma letra:" << endl;
+            cin >> letra;
 
-        cout << "Letra escolhida: ";
-        cout << letra << endl;
 
-            cout << "Letras acertadas: " << letrasAcertadas << endl;
-            cout << "Letras falhadas: "  << letraFalhadas << endl;
+        cout << "Letra escolhida: " << letra << endl;
 
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < palavra_escolhida.length(); i++)
         {
-            if (letra[0] == palavraSorteada[i])
+            if (letra[0] == palavra_escolhida[i])
             {
-                cout << "acertou na letra " << (i + 1) << endl;
-                palavraEscondida[i] = palavraSorteada[i];
+                cout << (i + 1) << ":" << "SIM | ";
+                palavra_escondida[i] = palavra_escolhida[i];
                 letrasAcertadas++;
-
             }
-            else if (letra[0] != palavraSorteada[i])
+            else if (letra[0] != palavra_escolhida[i])
             {
-                cout << "falhou na letra " << (i + 1) << endl;
-                letraFalhadas++;
+                cout << (i + 1) << ":" << "NAO | ";
+                letrasFalhadas++;
             }
         }
 
-        cout << "Letras acertadas: " << letrasAcertadas << endl;
-        cout << "Letras falhadas: " << letraFalhadas << endl;
+        cout << "\n" << "Letras acertadas: " << letrasAcertadas << endl;
+        cout << "Letras falhadas: " << letrasFalhadas << endl;
+        system("pause");
+        system("cls");
 
-        int comparar = palavraSorteada.compare(palavraEscondida);
+        int comparar = palavra_escolhida.compare(palavra_escondida);
 
         if (letrasAcertadas == 0)
         {
@@ -80,21 +241,82 @@ int main()
         }
 
         letrasAcertadas = 0;
-        letraFalhadas = 0;
+        letrasFalhadas = 0;
 
         if (comparar == 0)
         {
-            cout << "GAME OVER...acertou na palavra...a palavra era: " << palavraSorteada << endl;
+            cout << "BOM JOGO...acertou na palavra...a palavra era: " << palavra_escolhida << endl;
+             system("pause");
+            system("cls");
             break;
         }
 
         if (tentativasFalhadas == 8)
         {
             cout << "GAME OVER...nao acertou na palavra...." << endl;
+            system("pause");
+            system("cls");
             break;
         }
+    }
+    letrasFalhadas = 0;
+    letrasAcertadas = 0;
+    tentativasFalhadas = 0;
+}
 
-    } 
 
+int main(void)
+{
+    int opcao_menu_inicial;
 
+    opcao_menu_inicial = menu_inicial();
+
+    while (opcao_menu_inicial != 0)
+    {
+        switch (opcao_menu_inicial)
+        {
+        case 1: // Escolher tema
+            tema_escolhido = escolher_tema();
+            cout << "Já no main: " << tema_escolhido << endl;
+            system("pause");
+            cout << "Primeiro passo completo..." << endl;
+            cout << "Decidido o tema, o programa vai escolher uma palavra em 10 possíveis..." << endl;
+            system("pause");
+            palavra_escolhida = escolher_palavra();
+            cout << "Palavra escolhida (main): " << palavra_escolhida << endl;
+            system("pause");
+            cout << "Olá!...agora vamos começar o jogo :) " << endl;
+            system("pause");
+            system("cls");
+            jogo();
+            break;
+
+        case 2: // Decidir aleatoriamente
+            tema_escolhido = tema_aleatorio();
+            cout << "Já no main: " << tema_escolhido << endl;
+            system("pause");
+            cout << "Primeiro passo completo..." << endl;
+            cout << "Sorteado o tema, o programa vai escolher uma palavra em 10 possíveis..." << endl;
+            system("pause");
+            palavra_escolhida = escolher_palavra();
+            cout << "Palavra escolhida (main): " << palavra_escolhida << endl;
+            system("pause");
+            cout << "Olá!...agora vamos começar o jogo :) " << endl;
+            system("pause");
+            system("cls");
+            jogo();
+            break;
+
+        case 3: // Carregar jogo gravado
+            cout << "Carregar jogo gravado" << endl;
+            carregarJogo();
+            cout << "Jogo carregado..." << endl;
+            jogo();
+            break;
+
+        case 0:
+        break;
+        }
+        opcao_menu_inicial = menu_inicial();   
+    }
 }
